0dfbb17606ef55d09b844aa999e270ab6739fff5
98hub <akun.sumberweb@gmail.com>
2025-07-31 11:14:25 +0700

    fix logs error biar rapih

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
             .
           </li>
           <li className="mb-2 tracking-[-.01em]">
-            {/* Save and see your changes instantly! */}
-            Hello World!
+            Save and see your changes instantly!
+            {/* Hello World! */}
           </li>
           <li className="tracking-[-.01em]">
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
